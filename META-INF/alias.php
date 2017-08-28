74a039374320cb4daca009b5a882c692ef81e9e8
xiaohengjin <xiaohengjin@youzan.com>
2017-08-28 15:15:08 +0800

    Di

--- META-INF/alias.php
+++ META-INF/alias.php
@@ -3,7 +3,6 @@
 
 
 return [
-    \ZanPHP\Testing\Stub::class => "\\Zan\\Framework\\Testing\\Stub",
     \ZanPHP\Testing\TaskTest::class => "\\Zan\\Framework\\Testing\\TaskTest",
     \ZanPHP\Testing\UnitTest::class => "\\Zan\\Framework\\Testing\\UnitTest",
 ];
