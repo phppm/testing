a63046aebe046297fc6b7372ff9768f391203d77
huye <huye@youzan.com>
2017-09-05 20:13:45 +0800

    优化命名空间，支持IDE跳转

--- META-INF/.ide_helper/TaskTest.php
+++ META-INF/.ide_helper/TaskTest.php
@@ -2,109 +2,33 @@
 
 namespace Zan\Framework\Testing;
 
-use Zan\Framework\Foundation\Coroutine\Event;
-use Zan\Framework\Foundation\Coroutine\Task;
-use Zan\Framework\Network\Connection\ConnectionInitiator;
-use Zan\Framework\Store\Database\Sql\SqlMapInitiator;
-use Zan\Framework\Store\Database\Sql\Table;
-use Zan\Framework\Utilities\DesignPattern\Context;
-use Zan\Framework\Utilities\Types\Time;
-
 class TaskTest extends UnitTest
 {
-    public static $isInitialized = false;
-    public static  $isRunningJob = false;
-    private static $jobs = [];
-    private static $nTasks = 0;
+    private $TaskTest;
 
-    public $event = null;
-    public $eventChain = null;
-    protected $taskMethodPattern = '/^task.+/i';
-    protected $taskCounter = 0;
-    protected $coroutines = [];
+    public function __construct()
+    {
+        $this->TaskTest = new \ZanPHP\Testing\TaskTest();
+    }
 
     public function testTasksWork()
     {
-        self::$nTasks++;
-        self::$jobs[] = $this;
-
-        if (self::$isRunningJob) {
-            return;
-        }
-
-        $task = array_shift(self::$jobs);
-        $task->initTask();
-        $task->taskCounter++;
-        $task->eventChain->before('test_task_num_' . $task->taskCounter, 'test_task_done');
-        $task->scanTasks();
-        $taskCoroutine = $task->runTaskTests();
-        self::$isRunningJob = true;
-        $context = new Context();
-        $context->set('request_time', Time::stamp());
-        $request_timeout = 30;
-        $context->set('request_timeout', $request_timeout);
-        Task::execute($taskCoroutine, $context);
+        $this->TaskTest->testTasksWork();
     }
     
     protected function scanTasks()
     {
-        $ref = new \ReflectionClass($this);
-        $methods = $ref->getMethods(\ReflectionMethod::IS_PROTECTED | \ReflectionMethod::IS_PUBLIC);
-
-        foreach ($methods as $method) {
-            $methodName = $method->getName();
-            if (!preg_match($this->taskMethodPattern, $methodName)) {
-                continue;
-            }
-
-            $coroutine = $this->$methodName();
-            $this->coroutines[] = $coroutine;
-        } 
+        $this->TaskTest->scanTasks();
     }
 
     protected function initTask()
     {
-        if (!self::$isInitialized) {
-            //sql map
-            SqlMapInitiator::getInstance()->init();
-            //table
-            Table::getInstance()->init();
-            //connection pool init
-            ConnectionInitiator::getInstance()->init('connection', null);
-            self::$isInitialized = true;
-        }
-
-        $this->event = new Event();
-        $this->eventChain = $this->event->getEventChain();
-        
-        $this->event->bind('test_task_done', function () {
-            --self::$nTasks;
-            if (self::$jobs == []) {
-                self::$isRunningJob = false;
-            } else {
-                $task = array_shift(self::$jobs);
-                $task->initTask();
-                $task->taskCounter++;
-                $task->eventChain->before('test_task_num_' . $task->taskCounter, 'test_task_done');
-                $task->scanTasks();
-                $taskCoroutine = $task->runTaskTests();
-                $context = new Context();
-                $context->set('request_time', Time::stamp());
-                $request_timeout = 30;
-                $context->set('request_timeout', $request_timeout);
-                Task::execute($taskCoroutine, $context);
-                return;
-            }
-            if (self::$nTasks == 0) {
-                swoole_event_exit();
-            }
-        });
+        $this->TaskTest->initTask();
     }
     
     protected function runTaskTests()
     {
-        yield parallel($this->coroutines);
-        $this->event->fire('test_task_done');
+        $this->TaskTest->runTaskTests();
     }
 
 }
--- META-INF/.ide_helper/UnitTest.php
+++ META-INF/.ide_helper/UnitTest.php
@@ -4,31 +4,26 @@ namespace Zan\Framework\Testing;
 
 class UnitTest extends \PHPUnit_Framework_TestCase
 {
+    private $UnitTest;
+
+    public function __construct()
+    {
+        $this->UnitTest = new \ZanPHP\Testing\UnitTest();
+    }
+
     protected function invoke(&$object, $methodName, array $parameters = [])
     {
-        $reflection = new \ReflectionClass(get_class($object));
-        $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
-
-        return $method->invokeArgs($object, $parameters);
+        $this->UnitTest->invoke(&$object, $methodName,$parameters);
     }
 
     protected function getProperty(&$object, $propertyName)
     {
-        $reflection = new \ReflectionClass(get_class($object));
-        $property = $reflection->getProperty($propertyName);
-        $property->setAccessible(true);
-
-        return $property->getValue($object);
+        $this->UnitTest->getProperty(&$object, $propertyName);
     }
 
     protected function setPropertyValue(&$object, $propertyName, $value)
     {
-        $reflection = new \ReflectionClass(get_class($object));
-        $property = $reflection->getProperty($propertyName);
-        $property->setAccessible(true);
-
-        return $property->setValue($object, $value);
+        $this->UnitTest->setPropertyValue(&$object, $propertyName, $value);
     }
 
 }
